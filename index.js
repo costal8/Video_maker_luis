674b240177e40e4354b60870a4568065c505584e
costal8 <luisinstrumentacao@hotmail.com>
2020-03-30 23:06:07 -0300

    FINALIZADO Perguntar pelo termo de busca

---
+++ index.js
@@ -0,0 +1,13 @@
+const readline = require('readline-sync') // ler dados de entrada do usuario
+
+function start() {
+    const content= {}
+    content.searchTerm = askAndReturnSearchTeam()
+    function askAndReturnSearchTeam() {        
+    
+        return readline.question('Type a Wikipedia search term: ')
+    }
+    console.log(content)
+}
+
+start()
